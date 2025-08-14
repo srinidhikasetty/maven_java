ff9bd0cc993946b40b27a25c767825e685709d73
Yamuna345 <yamunathotakuri345@gmail.com>
2025-08-14 15:45:16 +0530

    1st commit

---
+++ src/main/java/SElab/java1/App.java
@@ -0,0 +1,10 @@
+package SElab.java1;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! from Yamuna 24bd5a6613");
+    }
+}
